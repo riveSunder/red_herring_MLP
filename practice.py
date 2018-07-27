baad26fe634e364de228d827e08932834d737dc6
riveSunder <riveSunder@protonmail.com>
2018-07-27 08:16:02 +0100

    first commit

---
+++ practice.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pylplot as plt
+import imagio as io
+import time 
+
+# Have tensorflow 1.4 and python 3.x installed
+# know how to make a multi-level perceptron classifier
+# know how to get variable from a graph
+# Review tensorboard 
+
+import tensorflow as tf
